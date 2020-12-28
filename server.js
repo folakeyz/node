15e7324fd3c99b2b2163a9f34cdf3870aa028eea
unknown <obaafemi@lotusbetaanalytics.com>
2020-12-28 18:43:16 +0000

    serverside

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors")
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    `mongodb+srv://iCode:LOXmw78dTJLBc5KJ@cluster0.9wfwq.mongodb.net/ecommerce?retryWrites=true&w=majority`, 
+{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log('Database Connected')
+});
+
+//middleware
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => 
+res.status(200).send("Hello World") 
+);
+
+app.listen(8001, ()=>{
+    console.log("running on port 3001");
+}); 
